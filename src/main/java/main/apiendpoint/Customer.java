88657972fd2ae54751610e528e5e4d98c6deecfc
Tyler Tracy <me@tylertracy.com>
2019-12-10 13:24:31 -0600

    More things
    Co-authored-by: Beau Cross <beau_cross@yahoo.com>
    Co-authored-by: Luke Brandon <lukebrandon68@gmail.com>

---
+++ src/main/java/main/apiendpoint/Customer.java
@@ -0,0 +1,75 @@
+package main.apiendpoint;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer customerId;
+  private Integer accountId;
+  private Integer phone;
+  private String name;
+  private String email;
+  private String password;
+  private Integer addressId;
+
+  public Integer getCustomerId() {
+    return this.customerId;
+  }
+
+  public void setCustomerId(Integer customerId) {
+    this.customerId = customerId;
+  }
+
+  public Integer getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(Integer accountId) {
+    this.accountId = accountId;
+  }
+
+  public Integer getPhone() {
+    return this.phone;
+  }
+
+  public void setPhone(Integer phone) {
+    this.phone = phone;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Integer getCustomerId() {
+    return this.customerId;
+  }
+
+  public void setCustomerId(Integer customerId) {
+    this.customerId = customerId;
+  }
+
+  public Integer getCustomerId() {
+    return this.customerId;
+  }
+
+  public void setCustomerId(Integer customerId) {
+    this.customerId = customerId;
+  }
+}
--- src/main/java/main/apiendpoint/HelloController.java
+++ src/main/java/main/apiendpoint/HelloController.java
@@ -2,9 +2,11 @@ package main.apiendpoint;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin
 @RestController
 public class HelloController {
   @Autowired
